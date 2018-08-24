5925dae323ecd369fe71649773505a5105068b73
http://jneen.net/ <jneen@jneen.net>
2018-08-23 18:46:11 -0700

    remove the use of thread variables in simple.rb

--- notes/simple.rb
+++ notes/simple.rb
@@ -1,14 +1,22 @@
 require 'thread'
 require 'set'
 
+$exception = StandardError.new
+
 # $... means global variable
 
 $mutex = Mutex.new
 $readers = Set.new
 $writers = Set.new
 $block_type = :none
-$block_set = []
 $open = true
+
+# an element of the block set will look like:
+# [thread, value]
+# where value is either the value to be written (if writers are blocked)
+# or a ref to receive the value (if readers are blocked)
+$block_set = []
+
 
 $output_mutex = Mutex.new
 $output = []
@@ -24,25 +32,25 @@ end
 def rm_reader(t)
   to_close = $mutex.synchronize do
     if $block_type == :read
-      $block_set.delete(t)
+      $block_set.reject! { |b| b[0] == t }
     end
 
     cleanup_from(t, $readers)
   end
 
-  to_close.each { |thread| thread.raise(Interrupt.new) }
+  to_close.each { |thread| thread.raise($exception) }
 end
 
 def rm_writer(t)
   to_close = $mutex.synchronize do
     if $block_type == :write
-      $block_set.delete(t)
+      $block_set.reject! { |b| b[0] == t }
     end
 
     cleanup_from(t, $writers)
   end
 
-  to_close.each { |thread| thread.raise(Interrupt.new) }
+  to_close.each { |thread| thread.raise($exception) }
 end
 
 def cleanup_from(t, set)
@@ -59,7 +67,7 @@ def cleanup_from(t, set)
     $open = false
     to_clean = $block_set.dup
     $block_set.clear
-    return to_clean
+    return to_clean.map { |b| b[0] }
   else
     return Set.new
   end
@@ -68,7 +76,7 @@ end
 def read(t)
   $mutex.synchronize do
     if not $open
-      t.raise(Interrupt.new)
+      t.raise($exception)
     end
 
     case $block_type
@@ -76,60 +84,57 @@ def read(t)
       # in this case there are no waiting write threads,
       # so this process must block.
       $block_type = :read
-      $block_set << t
+      ref = [t, nil]
+      $block_set << ref
 
       # this releases the mutex and sleeps in one atomic action.
       # on wakeup, it will acquire the mutex again (and immediately
       # release it at the end of the synchronize block).
       $mutex.sleep
+
+      return ref[1]
     when :write
       # in this case there are waiting write threads,
       # so we do *not* block, and instead wake up a
       # write thread.
-      write_thread = $block_set.shift
+      write_thread, written_value = $block_set.shift
 
       if $block_set.empty?
         $block_type = :none
       end
 
-      # the square brackets on threads set thread-local variables.
-      # i suspect that if these were replaced by global variables
-      # then race conditions would be introduced.
-      t[:read_value] = write_thread[:write_value]
-
       write_thread.run
+
+      return written_value
     end
   end
-
-  return t[:read_value]
 end
 
 def write(t, val)
   $mutex.synchronize do
     if not $open
-      t.raise(Interrupt.new)
+      t.raise($exception)
     end
 
     case $block_type
     when :none, :write
       $block_type = :write
-      $block_set << t
-      t[:write_value] = val
+      $block_set << [t, val]
       $mutex.sleep
     when :read
-      read_thread = $block_set.shift
+      read_ref = $block_set.shift
 
       if $block_set.empty?
         $block_type = :none
       end
 
-      # here we mutate a thread variable on the *other* thread
+      # here we mutate the ref from the block_set
       # before waking it up again, so that it can receive the
       # value when it wakes up. we guarantee that at the end
       # of the synchronize block in #read, this value is set.
-      read_thread[:read_value] = val
+      read_ref[1] = val
 
-      read_thread.run
+      read_ref[0].run
     end
   end
 end
@@ -175,6 +180,11 @@ consumer3 = spawn_consumer!
 consumer1.join
 consumer2.join
 consumer3.join
+
+# the "rescue"s are necessary here because .join will re-raise the Inter
+# exception in our thread
+producer1.join rescue nil
+producer2.join rescue nil
 
 puts "output: #{$output.inspect}"
 puts "live threads (should be exactly 1, in \"run\" state):"
