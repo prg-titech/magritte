c30ead3b4d70f50b4a29401182dc48faa4bd8071
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-10-19 14:54:38 +0900

    Implement lexer and test for it
    There might be some cleanup needed and
    add additional lexing capabilities. But
    overall it should work now.

--- lib/magritte/lexer.rb
+++ lib/magritte/lexer.rb
@@ -25,6 +25,12 @@ module Magritte
         end
       end
 
+      # Object equality based on 
+      # https://stackoverflow.com/questions/1931604/whats-the-right-way-to-implement-equality-in-ruby
+      def ==(o)
+        o.class == self.class && o.type == @type && o.value == @value
+      end
+
       def inspect
         "#<#{self.class.name} #{self.repr}>"
       end
@@ -61,12 +67,61 @@ module Magritte
       elsif scan /\]/
         skip_ws
         return token(:rbrack)
+     elsif scan /\=>/
+       skip_ws
+       return token(:arrow)
+     elsif scan /[=]/
+       skip_ws
+       return token(:equal)
+     elsif scan /</
+       skip_ws
+       return token(:read_from)
+     elsif scan />>/
+       skip_ws
+       return token(:write_no_close)
+     elsif scan />/
+       skip_ws
+       return token(:write_to)
+     elsif scan /%%!/
+       skip_ws
+       return token(:per_per_excl)
+     elsif scan /%%/
+       skip_ws
+       return token(:per_per)
+     elsif scan /!!/
+       skip_ws
+       return token(:excl_excl)
+     elsif scan /&&/
+       skip_ws
+       return token(:amp_amp)
+     elsif scan /\|\|/
+       skip_ws
+       return token(:bar_bar)
+     elsif scan /&/
+       skip_ws
+       return token(:amp)
+     elsif scan /\|/
+       skip_ws
+       return token(:bar)
       elsif scan /[$](\w+)/
         skip_ws
-        binding.pry
         return token(:var, group(1))
+      elsif scan /[%](\w+)/
+        skip_ws
+        return token(:lex_var, group(1))
+      elsif scan /[?](\w+)/
+        skip_ws
+        return token(:bind, group(1))
+      elsif scan /([-]?[0-9]+([\.][0-9]*)?)/
+        skip_ws
+        return token(:num, group(1))
+      elsif scan /"((?:\\.|[^"])*)"/
+        skip_ws
+        return token(:bareword, group(1))
+      elsif scan /([a-zA-Z\-]+)/
+        skip_ws
+        return token(:bare, group(1))
       else
-        binding.pry
         raise "Unknown token"
       end
     end
--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -3,6 +3,12 @@ describe Magritte::Lexer do
   let(:lex) { Magritte::Lexer.new(input) }
   let(:tokens) { lex.to_a }
 
+  # Define helper functions for setting up tests
+  # Example: Magritte::AST::Variable["foo"] = _variable("foo")
+  define_method("_token") do |type, value = nil, range = nil|
+    Magritte::Lexer::Token.new(type, value, range)
+  end
+
   describe "some delimiters" do
     let(:input) {
       """
@@ -11,8 +17,93 @@ describe Magritte::Lexer do
     }
 
     it "parses basic delimiters" do
-      binding.pry
-      assert { tokens == nil }
+      assert { tokens == [_token(:lparen), _token(:lbrack), _token(:var,"hoge"), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "some basic keywords" do
+    let(:input) {
+      """
+      [({} )   ]    = %a
+      """
+    }
+
+    it "parses basic keywords" do
+      assert { tokens == [_token(:lbrack), _token(:lparen), _token(:lbrace), _token(:rbrace), _token(:rparen), _token(:rbrack), _token(:equal), _token(:lex_var, "a"), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "function header" do
+    let(:input) {
+      """
+      (func ?n)
+      """
+    }
+
+    it "parses basic function header" do
+      assert { tokens == [_token(:lparen), _token(:bare, "func"), _token(:bind, "n"), _token(:rparen), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "error recovery tokens" do
+    let(:input) {
+      """
+      ||    &&  !! %%%%!&&
+      """
+    }
+
+    it "parses tokens correctly" do
+      assert { tokens == [_token(:bar_bar), _token(:amp_amp), _token(:excl_excl), _token(:per_per), _token(:per_per_excl), _token(:amp_amp), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "oprators" do
+    let(:input) {
+      """
+      |     &  ==   =>  >>
+      """
+    }
+
+    it "parses operators" do
+      assert { tokens == [_token(:bar), _token(:amp), _token(:equal), _token(:equal), _token(:arrow), _token(:write_no_close), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "numbers" do
+    let(:input) {
+      """
+      2     6.28    0.00001   1.   -5.4
+      """
+    }
+
+    it "parses numbers correctly" do
+      assert { tokens == [_token(:num, "2"), _token(:num, "6.28"), _token(:num, "0.00001"), _token(:num, "1."), _token(:num,"-5.4"), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "strings" do
+    let(:input) {
+      """
+      \"asksnz-zwjdfqw345 r8 ewn    ih2wu\\\" wihf002+4-r9+***.m.-< \\\"\"
+      """
+    }
+
+    it "parses strings correctly" do
+      assert { tokens == [_token(:bareword, "asksnz-zwjdfqw345 r8 ewn    ih2wu\\\" wihf002+4-r9+***.m.-< \\\""), _token(:nl), _token(:eof)] }
+    end
+  end
+
+  describe "channel semantics" do
+    let(:input) {
+      """
+      channel = (make-channel)
+      reader < $channel
+      writer > $channel
+      """
+    }
+
+    it "parses channel semantics" do
+      assert { tokens == [_token(:bare, "channel"), _token(:equal), _token(:lparen), _token(:bare, "make-channel"), _token(:rparen), _token(:nl), _token(:bare, "reader"), _token(:read_from), _token(:var, "channel"), _token(:nl), _token(:bare, "writer"), _token(:write_to), _token(:var, "channel"), _token(:nl), _token(:eof)] }
     end
   end
 end
