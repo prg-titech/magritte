5b518d2b839e5f30c4df1c0c345a0927d4fc3684
http://jneen.net/ <jneen@jneen.net>
2018-11-10 11:54:55 +0900

    add Collector#wait_for_close

--- lib/magritte/channel.rb
+++ lib/magritte/channel.rb
@@ -244,12 +244,27 @@ module Magritte
     def initialize
       @collection = []
       @mutex = Mutex.new
+      @writers = Set.new
+      @close_waiters = []
+      @open = true
     end
 
-    def add_writer(*); end
     def add_reader(*); end
     def remove_reader(*); end
-    def remove_writer(*); end
+
+    def remove_writer(p)
+      action = @mutex.synchronize do
+        next :nop unless @open
+        @writers.delete(p)
+
+        next :nop unless @writers.empty?
+
+        @open = false
+        :close
+      end
+
+      @close_waiters.each { |t| t.run } if action == :close
+    end
 
     def read
       raise 'not readable'
@@ -258,10 +273,13 @@ module Magritte
     def write(val)
       @mutex.synchronize { @collection << val }
     end
-  private
 
-    def close!
-      # pass
+    def wait_for_close
+      @mutex.synchronize do
+        next unless @open
+        @close_waiters << Thread.current
+        @mutex.sleep
+      end
     end
 
     def inspect
