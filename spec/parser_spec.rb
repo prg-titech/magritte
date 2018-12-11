d415bbd8f0d34c4780a16402b312d8985ca49020
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-12-11 20:11:49 +0900

    Remove too big tests from parser spec

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -33,57 +33,6 @@ describe Magritte::Parser do
     end
   end
 
-  describe "variables" do
-    let(:input) {
-      """
-      $x = 1
-      %n = 2
-      """
-    }
-
-    #it "parses correctly" do
-    #  assert { ast.elems.size == 2 }
-    #  assert { ast.elems.all?(Magritte::AST::Assignment) }
-    #  #assert { ast.elems[0].lhs.inspect == "#<Magritte::AST::Variable[\"x\"]>" }
-    #  assert { ast.elems[0].rhs.inspect == "#<Magritte::AST::Number[\"1\"]>" }
-    #  assert { ast.elems[1].lhs.inspect == "#<Magritte::AST::LexVariable[\"n\"]>" }
-    #  assert { ast.elems[1].rhs.inspect == "#<Magritte::AST::Number[\"2\"]>" }
-    #end
-  end
-
-  describe "command" do
-    let(:input) {
-      """
-      multi-arg-command arg1 arg2 >$out <$in
-      """
-    }
-
-    #it "parses correctly" do
-    #  assert { ast.elems.size == 1 }
-    #  assert { ast.elems.first.is_a?(Magritte::AST::Command) }
-    #  assert { ast.elems.first.vec.inspect == "[#<Magritte::AST::String[\"multi-arg-command\"]>, #<Magritte::AST::String[\"arg1\"]>, #<Magritte::AST::String[\"arg2\"]>]" }
-    #  assert { ast.elems.first.redirects.inspect == "[#<Magritte::AST::Redirect[:<, *#<Magritte::AST::Variable[\"in\"]>]>, #<Magritte::AST::Redirect[:>, *#<Magritte::AST::Variable[\"out\"]>]>]" }
-    #end
-  end
-
-  describe "lambda" do
-    let(:input) {
-      """
-      f = (?x ?y => add x y)
-      """
-    }
-
-    #it "parses correctly" do
-    #  assert { ast.elems.size == 1 }
-    #  assert { ast.elems[0].is_a?(Magritte::AST::Assignment) }
-    #  #assert { ast.elems[0].lhs.inspect == "#<Magritte::AST::String[\"f\"]>" }
-    #  assert { ast.elems[0].rhs.is_a?(Magritte::AST::Lambda) }
-    #  assert { ast.elems[0].rhs.name == "anon@test@1:11~1:29" }
-    #  assert { ast.elems[0].rhs.patterns.inspect == "[#<Magritte::AST::Binder[\"x\"]>, #<Magritte::AST::Binder[\"y\"]>]" }
-    #  assert { ast.elems[0].rhs.bodies.inspect == "[#<Magritte::AST::Command[**[#<Magritte::AST::String[\"add\"]>, #<Magritte::AST::String[\"x\"]>, #<Magritte::AST::String[\"y\"]>], **[]]>]" }
-    #end
-  end
-
   describe "pipe" do
     let(:input) {
       """
